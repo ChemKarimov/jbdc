af168c1f784ce1b34e4a6536792d601bc2bd68d6
ChemKarimov <chemkarimov@yandex.ru>
2024-11-26 21:40:29 +0300

    Задача 1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,17 @@
 package jm.task.core.jdbc;
-
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserDaoJDBCImpl jdbc = new UserDaoJDBCImpl();
+
+        jdbc.createUsersTable();
+        jdbc.saveUser("Ivan", "Ivanov", (byte) 20);
+        jdbc.saveUser("Danil", "Danilov", (byte) 21);
+        jdbc.saveUser("Aboba", "Valeryev", (byte) 24);
+        jdbc.saveUser("Denis", "Petrov", (byte) 27);
+        jdbc.getAllUsers();
+        jdbc.cleanUsersTable();
+        jdbc.dropUsersTable();
     }
 }
